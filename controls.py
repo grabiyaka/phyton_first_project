498ffb38e53ee20871ee2f9abf5e62fd4e59c538
rabiyaka <illya.gerasimets@gmail.com>
2022-03-21 21:23:30 +0200

    update control

---
+++ controls.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+
+def events(gun):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                gun.mright = True
+            elif event.key == pygame.K_a:
+                gun.mleft = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                gun.mright = False
+            elif event.key == pygame.K_a:
+                gun.mleft = False
--- index.py
+++ index.py
@@ -1,19 +1,19 @@
-import pygame
-import sys
+import pygame, controls
 from gun import Gun
+
+ScWth = 700
+ScHght = 800
 
 def run():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((ScWth, ScHght))
     pygame.display.set_caption("Space War")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        controls.events(gun)
+        gun.update_gun()
         screen.fill(bg_color)
         gun.output()
         pygame.display.flip()
