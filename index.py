4505189363a399d54d910539c81d30954448f941
rabiyaka <illya.gerasimets@gmail.com>
2022-03-21 14:56:59 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+from gun import Gun
+
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Space War")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        gun.output()
+        pygame.display.flip()
+         
+
+run()
+
